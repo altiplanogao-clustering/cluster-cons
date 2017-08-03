c05b5e645a2724aed8885e7575effadbca37a314
Altiplano Gao <altiplanogao@hotmail.com>
2017-08-04 02:31:33 +0800

    Dynamic Inventory

--- roles/hadoop/scripts/mapred_demo.sh
+++ roles/hadoop/scripts/mapred_demo.sh
@@ -2,5 +2,5 @@
 $HADOOP_PREFIX/bin/hdfs dfs -mkdir /user
 $HADOOP_PREFIX/bin/hdfs dfs -mkdir /user/mapred
 $HADOOP_PREFIX/bin/hdfs dfs -put $HADOOP_PREFIX/etc/hadoop input
-$HADOOP_PREFIX/bin/hadoop jar $HADOOP_PREFIX/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.8.0.jar grep input output 'dfs[a-z.]+'
+$HADOOP_PREFIX/bin/hadoop jar $HADOOP_PREFIX/share/hadoop/mapreduce/hadoop-mapreduce-examples-{{ hadoop_conf.version }}.jar grep input output 'dfs[a-z.]+'
 $HADOOP_PREFIX/bin/hdfs dfs -cat output/*
